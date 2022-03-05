c74a62bb0c63f5cd53d6c1cabaacdd88daa49f1e
rahel seyum <rachiellseyum@gmail.com>
2022-03-05 13:40:59 -0800

    creating tab url and saving it

--- index.js
+++ index.js
@@ -2,34 +2,57 @@ let myLeads=[];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl=document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn=document.getElementById("tab-btn");
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+function render(data){
+    let listItems="";
+   for(let R=0;R<data.length;R++){
+        // listItems+="<li> <a target='_blank' href='"+myLeads[R]+"'>"+myLeads[R]+"</a> </li> "; 
+        //instead of this we can use this method
+        listItems+=`
+        <li>
+         <a target='_blank' href='${data[R]}'>
+         ${data[R]}
+         </a> 
+        </li>
+         `; 
+    
+    // localStorage.setItem("mezgeb",myLeads[R]);
+    // localStorage.clear(); 
+    //   console.log(localStorage.getItem("mezgeb"));
+} 
+ulEl.innerHTML=listItems;  
+}
+
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear();
+    myLeads=[];
+    render(myLeads);
+})
+const tabs=[
+    {url: "https://www.linkedin.com/"}
+]
+tabBtn.addEventListener("click", function(){
+myLeads.push(tabs[0].url);
+localStorage.setItem("myLeads",JSON.stringify(myLeads));
+render(myLeads);
+})
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
     inputEl.value=""; 
    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-   renderLeads();
-   console.log(localStorage.getItem("myLeads"));
-   let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
-   console.log("@",leadsFromLocalStorage ); 
+   render(myLeads);
+//    console.log(localStorage.getItem("myLeads"));
 });
-function renderLeads(){
-    let listItems="";
-  
-    for(let R=0;R<myLeads.length;R++){
-        // listItems+="<li> <a target='_blank' href='"+myLeads[R]+"'>"+myLeads[R]+"</a> </li> "; 
-        //instead of this we can use this method
-        listItems+=`
-        <li>
-         <a target='_blank' href='${myLeads[R]}'>${myLeads[R]}</a> 
-        </li>
-         `; 
-    ulEl.innerHTML=listItems;
-    // localStorage.setItem("mezgeb",myLeads[R]);
-    // localStorage.clear(); 
-    //   console.log(localStorage.getItem("mezgeb"));
-}   
-}
+
 //creat element
         //set text content
      //append to ul
